acdd412be3c3cdd44dea8e1296e47d1999177709
Jaronmutombo <kalungamutombo@gmail.com>
2026-08-31 18:38:13 -0600

    ok

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,19 +1,37 @@
+using System.IO.Pipelines;
+
 public static class Arrays
 {
-    /// <summary>
-    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
-    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
-    /// integer greater than 0.
-    /// </summary>
-    /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    public static double[] MultiplesOf(double number, int length)
+/// <summary>
+/// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+/// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+/// integer greater than 0.
+/// </summary>
+/// <returns>array of doubles that are the multiples of the supplied number</returns>
+
+public static double[] MultiplesOf(double startNumber, int count)
+{
+    // Create an array that will hold the requested number of multiples.
+    double[] multiples = new double[count];
+
+    // Loop through each position in the array.
+    for (int i = 0; i < count; i++)
     {
+        // Calculate the multiple and store it in the array.
+        multiples[i] = startNumber * (i + 1);
+    }
+
+    // Return the completed array.
+    return multiples;
+}
+
+        
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        ///return []; // replace this return statement with your own
     }
 
     /// <summary>
@@ -23,11 +41,12 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
-    public static void RotateListRight(List<int> data, int amount)
-    {
+///    public static void RotateListRight(List<int> data, int amount)
+///    {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
-    }
-}
+///    }
+    
+
