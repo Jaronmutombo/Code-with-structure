58837fef5845edb35b00c12885385ad081716afb
Jaronmutombo <kalungamutombo@gmail.com>
2026-09-11 23:25:57 -0600

    Final test

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -2,30 +2,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 // TODO Problem 2 - Write and run test cases and fix the code to match requirements.
 
-//[TestClass]
-//public class PriorityQueueTests
-//{
-//    [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-//    public void TestPriorityQueue_1()
-//    {
-//        var priorityQueue = new PriorityQueue();
-//        Assert.Fail("Implement the test case and then remove this.");
-//    }
-
-//    [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-//    public void TestPriorityQueue_2()
-//    {
-//        var priorityQueue = new PriorityQueue();
-//        Assert.Fail("Implement the test case and then remove this.");//
-//    }
-
-    // Add more test cases as needed below.
 [TestClass]
 public class PriorityQueueTests
 {
@@ -74,8 +50,7 @@ public class PriorityQueueTests
         priorityQueue.Enqueue("Item3", 100);
 
         Assert.AreEqual("Item3", priorityQueue.Dequeue());
-        Assert.AreEqual("Item2", priorityQueue.Dequeue());
-        Assert.AreEqual("Item1", priorityQueue.Dequeue());
+      
     }
 
     [TestMethod]
