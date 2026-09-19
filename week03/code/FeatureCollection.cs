e763cd99e381950997ccd1de620eefe9e2f2415c
Jaronmutombo <kalungamutombo@gmail.com>
2026-09-19 00:21:56 -0600

    GeoJSON

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,30 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
 public class FeatureCollection
+
+// TODO Problem 5 - ADD YOUR CODE HERE
+// Create additional classes as necessary
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public Feature[] Features { get; set; }
+
+}
+
+public class Feature
+{
+    public Geometry Geometry { get; set; }
+    public Properties Properties { get; set; }
+}
+
+public class Geometry
+{
+    public string Type { get; set; }
+    public double[] Coordinates { get; set; }
+}
+
+public class Properties
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
 }
